1a4e1f87e8bf043b3a2cd34afe3ba45c39631bd6
Martin Lau <martin.lau@permeance.com.au>
2012-10-31 18:17:47 +1100

    updated presentation of memory graph

--- src/main/webapp/js/memory.js
+++ src/main/webapp/js/memory.js
@@ -1,66 +1,101 @@
 "use strict";
-AUI().use("charts",
-          "console",
-          "datasource-io",
-          "json-parse",
-          function (A) {
-              A.log("creating charts...");
-              A.all(".memory-chart").each(function (node) {
 
-                  var clusterNodeId = node.attr("data-cluster-node-id");
-                  A.log("creating filter function for: " + clusterNodeId);
-                  var filterFn = function (item) {
-                      A.log("filtering: " + item);
-                      return item.clusterNodeId === clusterNodeId;
-                  };
+AUI().use("charts", "datasource-io", "json-parse", function (A) {
 
-                  A.log("creating map function");
-                  var mapFn = function (item) {
-                      A.log("mapping: " + item);
-                      return {
-                          date            : new Date(item.timestamp),
-                          'used heap'     : item.monitor.result.heap.used / (1024 * 1024),
-                          'used non-heap' : item.monitor.result.nonHeap.used / (1024 * 1024)
-                      };
-                  };
+  // Configuration for rendering the axes of the chart - see XXX line XXX for details
+  var axes = {
+    time : {
+      labelFormat : "%T"
+    }
+  };
 
-                  var jsonUrl = node.attr("data-json-url");
-                  A.log("creating datasource for: " + jsonUrl);
-                  var dataSource = new A.DataSource.IO({
-                                                           source : jsonUrl
-                                                       });
+  // Configuration of the series display of the chart - see charts-debug.js lines 7133 and 8548 for details
+  var styles = {
+    series : {
+      max   : {
+        marker : {
+          height : 3,
+          width  : 3
+        },
+        line   : {
+          weight : 2
+        }
+      },
+      total : {
+        marker : {
+          height : 3,
+          width  : 3
+        },
+        line   : {
+          weight : 2
+        }
+      },
+      used  : {
+        marker : {
+          height : 3,
+          width  : 3
+        },
+        line   : {
+          weight : 2
+        }
+      }
+    }
+  };
 
-                  A.log("sending request for data");
-                  dataSource.sendRequest({
-                                             request : "",
-                                             on      : {
-                                                 success : function (e) {
-                                                     A.log("handling success callback for: " + e);
+  A.all(".memory-chart").each(function (node) {
 
-                                                     var json = A.JSON.parse(e.response.results[0].responseText);
-                                                     A.log("parsed json: " + json);
+    // The filter predicate to apply to incoming history items to determine whether it should be included in the chart
+    var filterFn = function (item) {
+      return item.clusterNodeId === node.attr("data-cluster-node-id");
+    };
 
-                                                     var data = A.Array(json).filter(filterFn).map(mapFn);
-                                                     A.log("chart data: " + data);
+    // The map transformation to apply to an incoming history item to determine what should information be included in the chart
+    var mapFn = function (item) {
+      return {
+        time  : new Date(item.timestamp),
+        max   : item.monitor.result.heap.max,
+        total : item.monitor.result.heap.committed,
+        used  : item.monitor.result.heap.used
+      };
+    };
 
-                                                     A.log("creating chart for: " + node);
-                                                     var chart = new A.Chart({
-                                                                                 dataProvider : data,
-                                                                                 render       : node,
-                                                                                 categoryKey  : "date",
-                                                                                 categoryType : "time",
-                                                                                 axes         : {
-                                                                                     date : {
-                                                                                         labelFormat : "%T"
-                                                                                     }
-                                                                                 }});
+    // The function to call on data fetch success
+    var success = function (e) {
 
-                                                 },
-                                                 failure : function (e) {
-                                                     A.log("handling error callback for: " + e);
-                                                     alert("Oops something went wrong here.. " + e.error.message);
-                                                 }
-                                             }
-                                         });
-              });
-          });
+      // Parse the response text as JSON
+      var historyItems = A.JSON.parse(e.response.results[0].responseText);
+
+      // Then filter and map the resulting json as necessary for display
+      var dataSet = A.Array(historyItems).filter(filterFn).map(mapFn);
+
+      // And finally throw it into a chart using the earlier objects for configuration
+      new A.Chart({
+                    axes                : axes,
+                    categoryKey         : "time",
+                    categoryType        : "time",
+                    dataProvider        : dataSet,
+                    horizontalGridlines : true,
+                    render              : node,
+                    styles              : styles,
+                    verticalGridlines   : true
+                  });
+    };
+
+    // The function to call on data fetch failure
+    var failure = function (e) {
+      // If it failed - just show an alert and hope for the best
+      alert("Oops something went wrong here.. " + e.error.message);
+    };
+
+    // Create a data source referring back to the relevant node JSON url
+    var dataSource = new A.DataSource.IO({ source : node.attr("data-json-url") });
+
+    // Make chart go now
+    dataSource.sendRequest({
+                             on : {
+                               success : success,
+                               failure : failure
+                             }
+                           });
+  });
+});
