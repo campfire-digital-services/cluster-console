3a27e03b92170d76c49fb91d1d04ff03f65dd968
Martin Lau <martin.lau@permeance.com.au>
2012-11-02 17:24:36 +1100

    Refactored all charts to use the same common config.

--- src/main/webapp/js/classLoading.js
+++ src/main/webapp/js/classLoading.js
@@ -1,6 +1,3 @@
 AUI().use("cluster-console", function (A) {
-
-    A.ClusterConsole.chart(".classLoading-chart",
-                           ["Unloaded Class Count", "Loaded Class Count", "Total Loaded Class Count"]);
-
+    A.ClusterConsole.chart(".classLoading-chart", ["Unloaded Class Count", "Loaded Class Count", "Total Loaded Class Count"]);
 });
--- src/main/webapp/js/cluster-console.js
+++ src/main/webapp/js/cluster-console.js
@@ -1,86 +1,83 @@
-YUI.add("cluster-console", function (Y) {
-
-    "use strict";
-
-    Y.ClusterConsole = {
-
-        chart : function (selector, series) {
-
-            AUI().use("charts", "datasource-io", "datasource-jsonschema", function (A) {
-
-                A.all(selector).each(function (node) {
-
-                    var success = function (e) {
-
-                        var axes = {
-                            "Timestamp" : {
-                                labelFormat : "%T"
-                            }
-                        };
-
-                        var marker = {
-                            marker : {
-                                height : 4,
-                                width  : 4
-                            },
-                            line   : {
-                                weight : 3
-                            }
-                        };
-
-                        new A.Chart({
-                                        axes                : axes,
-                                        categoryKey         : "Timestamp",
-                                        categoryType        : "time",
-                                        dataProvider        : e.response.results,
-                                        horizontalGridlines : true,
-                                        render              : node,
-                                        styles              : {
-                                            series : {
-                                                "Unloaded Class Count"     : marker,
-                                                "Loaded Class Count"       : marker,
-                                                "Total Loaded Class Count" : marker
-                                            }
-                                        },
-                                        verticalGridlines   : true
-                                    });
-                    };
-
-                    var failure = function (e) {
-                        alert("Oops... something went wrong while rendering the chart: " + e);
-                    };
-
-                    var config = {
-                        source : node.attr("data-json-url")
-                    };
-
-                    var request = {
-                        request : "",
-                        on      : {
-                            success : success,
-                            failure : failure
-                        }
-                    };
-
-                    var schema = {
-                        schema : {
-                            resultFields      : [
-                                {
-                                    key    : "Timestamp",
-                                    parser : "date"
+"use strict";
+YUI.add("cluster-console",
+        function (Y) {
+            Y.log("Initialising ClusterConsole namespace", "debug");
+            Y.ClusterConsole = {
+                chart : function (selector, series) {
+                    Y.log("Creating chart for: " + selector + " with series: " + series, "info");
+                    Y.all(selector).each(function (node) {
+                        var success = function (e) {
+                            var axes = {
+                                "Timestamp" : {
+                                    labelFormat : "%T"
                                 }
-                            ].concat(series),
-                            resultListLocator : node.attr("data-cluster-node-id")
-                        }
-                    };
-
-                    new A.DataSource.IO(config).plug(A.Plugin.DataSourceJSONSchema, schema).sendRequest(request);
-
-                });
-
-            });
-
+                            };
+                            var marker = {
+                                marker : {
+                                    height : 4,
+                                    width  : 4
+                                },
+                                line   : {
+                                    weight : 3
+                                }
+                            };
+                            var styles = {
+                                series : {}
+                            };
+                            Y.Array(series).forEach(function (val) {
+                                Y.log("Configuring marker style for series: " + val, "debug");
+                                styles.series[val] = marker;
+                            });
+                            Y.log("Graphing data from: " + e, "debug");
+                            new Y.Chart({
+                                            axes                : axes,
+                                            categoryKey         : "Timestamp",
+                                            categoryType        : "time",
+                                            dataProvider        : e.response.results,
+                                            horizontalGridlines : true,
+                                            render              : node,
+                                            styles              : styles,
+                                            verticalGridlines   : true
+                                        });
+                        };
+                        var failure = function (e) {
+                            Y.log("Error fetching chart data: " + e, "error");
+                            alert("Oops... something went wrong while rendering the chart: " + e);
+                        };
+                        var config = {
+                            source : node.attr("data-json-url")
+                        };
+                        var request = {
+                            request : "",
+                            on      : {
+                                success : success,
+                                failure : failure
+                            }
+                        };
+                        var schema = {
+                            schema : {
+                                resultFields      : [
+                                    {
+                                        key    : "Timestamp",
+                                        parser : "date"
+                                    }
+                                ].concat(series),
+                                resultListLocator : node.attr("data-cluster-node-id")
+                            }
+                        };
+                        Y.log("Creating JSON request for data", "info");
+                        new Y.DataSource.IO(config).plug(Y.Plugin.DataSourceJSONSchema, schema).sendRequest(request);
+                    });
+                }
+            };
+        },
+        "0.1",
+        {
+            requires : [
+                "charts",
+                "datasource-io",
+                "datasource-jsonschema"
+            ]
         }
-    };
-}, "0.1", {requires : ["charts", "datasource-io", "datasource-jsonschema"]});
+);
 
